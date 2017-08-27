e065cf7309dce220ced8baf8a37683d5947fc160
jamesquietly <jamesquietly@gmail.com>
2017-08-26 23:30:18 -0700

    finished section 5

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,24 @@
 class Book
-# write your code here
+    attr_accessor :title
+
+    #capitalize words in a string
+    def capitalized string
+        little_words = ["a", "an", "the", "and", "in", "of"]
+        string = string.downcase
+        string = string.split(" ")
+        if string.length > 1
+            string.each_index do |i|
+                if i == 0 || !little_words.include?(string[i])
+                    string[i] = string[i].capitalize
+                end
+            end
+        else
+            string[0] = string[0].capitalize
+        end
+        string.join(" ").strip
+    end
+
+    def title=(string)
+        @title = capitalized(string)
+    end
 end
